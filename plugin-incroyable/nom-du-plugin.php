95035429ed510677dd7ab746bf77a877fe1e37bf
S-Thibault <sylvain.thibault@infamail.com>
2020-07-28 14:34:20 +0200

    Add files via upload

---
+++ plugin-incroyable/nom-du-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Mon premier plugin
+Plugin URI: https://mon-siteweb.com/
+Description: Ceci est mon premier plugin
+Author: Mon nom et prénom ou celui de ma société
+Version: 1.0
+Author URI: http://mon-siteweb.com/
+*/
